bc29f53cb509623492c35a5b476587ddc735b602
Oussama Rahmouni <ousrah@hotmail.com>
2022-02-14 09:27:23 +0100

    Ajoutez des fichiers projet.

---
+++ ModeConnecte203/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ModeConnecte203
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection cn = new SqlConnection(@"data source=.\sqlserver2017;initial catalog=librairie;user id=sa;Password=P@ssw0rd");
+            cn.Open();
+            SqlCommand com = new SqlCommand("select * from ouvrage",cn);
+            SqlDataReader dr = com.ExecuteReader();
+            listBox1.Items.Clear();
+            while (dr.Read())
+            {
+                listBox1.Items.Add(dr["nomouvr"]);
+                listBox3.Items.Add(dr["numouvr"]);
+            }
+             dr.Close();
+            com.CommandText = "select * from editeur";
+
+            dr = com.ExecuteReader();
+            listBox2.Items.Clear();
+            while (dr.Read())
+            {
+                listBox2.Items.Add(dr["nomed"]);
+            }
+
+
+       
+            dr = null;
+            com = null;
+            cn.Close();
+            cn = null;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SqlConnection cn = new SqlConnection(@"data source=.\sqlserver2017;initial catalog=librairie;user id=sa;Password=P@ssw0rd");
+            cn.Open();
+            SqlCommand com = new SqlCommand("select count(*) from ouvrage", cn);
+            int nb = Convert.ToInt32(com.ExecuteScalar());
+            textBox1.Text = nb.ToString() ;
+            com = null;
+            cn.Close();
+            cn = null;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SqlConnection cn = new SqlConnection(@"data source=.\sqlserver2017;initial catalog=librairie;user id=sa;Password=P@ssw0rd");
+            cn.Open();
+            SqlCommand com = new SqlCommand("insert into ouvrage (numouvr,nomed, nomouvr,numrub, anneeparu) values (54216521,'CLET','test mode connecte',1,2022)", cn);
+            com.ExecuteNonQuery();
+            MessageBox.Show("ouvrage ajouté avec success");
+            cn.Close();
+            cn = null;
+
+        }
+    }
+}
---
+++ ModeConnecte203/Form2.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ModeConnecte203
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection cn = new SqlConnection(@"data source=.\sqlserver2017;initial catalog=librairie;user id=sa;Password=P@ssw0rd");
+            cn.Open();
+            SqlCommand com = new SqlCommand("select * from ouvrage", cn);
+            SqlDataReader dr = com.ExecuteReader();
+
+            DataTable tbl = new DataTable();
+            DataColumn numouvr = new DataColumn("numouvr",typeof(int));
+            DataColumn nomouvr = new DataColumn("nomouvr", typeof(string));
+            tbl.Columns.Add(numouvr);
+            tbl.Columns.Add(nomouvr);
+            while (dr.Read())
+            {
+                DataRow r = tbl.NewRow();
+                r[0] = dr["numouvr"];
+                r[1] = dr["nomouvr"];
+                tbl.Rows.Add(r);
+           }
+
+
+            listBox1.DisplayMember = "nomouvr";
+            listBox1.ValueMember = "numouvr";
+            listBox1.DataSource = tbl;
+
+            dr.Close();
+
+            dr = null;
+            com = null;
+            cn.Close();
+            cn = null;
+        }
+    }
+}
